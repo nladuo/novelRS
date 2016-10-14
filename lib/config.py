82fbd4fcd7476216b8488cd67ed80c88856e62e6
nladuo <kalen25115@gmail.com>
2016-10-14 11:35:57 +0800

    modified:   lib/config.py

--- lib/config.py
+++ lib/config.py
@@ -6,6 +6,6 @@ config = {
     'db_pass': '',          # mongo的密码
     'db_host': 'localhost',
     'db_port': 27017,
-    'db_name': 'novel_rs',
+    'db_name': 'novelRS',
     'cpu_num': 4            # 并行分词的CPU数目
 }
