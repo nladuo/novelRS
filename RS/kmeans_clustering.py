4e9793d6f017831690e8bbc1283b0220558d93d3
nladuo <kalen25115@gmail.com>
2017-04-19 23:41:54 +0800

    modified:   kmeans_clustering.py

--- RS/kmeans_clustering.py
+++ RS/kmeans_clustering.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     with open("./dataset.pickle", "rb") as f:
         X = np.load(f)
         print("shape of dataset:", X.shape)
-
-    km = KMeans(init='k-means++', n_clusters=2, verbose=1)
+    n_clusters = X.shape[0] / 50
+    km = KMeans(init='k-means++', n_clusters=n_clusters, verbose=1)
     t0 = time()
     km.fit(X)
     print("done in %0.3fs" % (time() - t0))
